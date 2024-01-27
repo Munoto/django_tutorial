0df84900305818012cf105f0144558dad31c87bf
Munoto <zeron.masimov@mail.ru>
2024-01-27 11:37:53 +0600

    New template added

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import index, indexItem
+from myapp.views import index, indexItem, add_item
 
 urlpatterns = [
     path('', index),
     path('<int:my_id>/', indexItem, name = "detail"),
+    path('additem/', add_item, name = "add_item"),
 ]
